43e1e7221046cc18276a753e660cf1a3cfaf1d17
rishabh <145670392+wrisabhh@users.noreply.github.com>
2025-12-30 10:22:45 +0530

    Time: 437 ms (100.00%) | Memory: 0B (100.00%) - LeetSync

---
+++ 608-tree-node/tree-node.sql
@@ -0,0 +1,7 @@
+/* Write your PL/SQL query statement below */
+/* Write your PL/SQL query statement below */
+select id,case when p_id is null then 'Root'
+
+when id  in(select distinct(p_id) from tree ) 
+
+then 'Inner' else 'Leaf' end as type from tree ;
