6905b6ce62b0168d94eb4d03bbbb85f5c230d25e
rishabh <145670392+wrisabhh@users.noreply.github.com>
2026-01-01 10:24:24 +0530

    Time: 702 ms (44.75%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -0,0 +1,11 @@
+/* Write your PL/SQL query statement below */
+select round(
+    sum(
+        case when order_date=customer_pref_delivery_date then 1 else 0 end)*100/count(*),2)
+        as immediate_percentage from  Delivery where (customer_id, order_date) IN
+         (
+            select customer_id,min(order_date)
+            from Delivery 
+            group by customer_id
+        ) ;
+
